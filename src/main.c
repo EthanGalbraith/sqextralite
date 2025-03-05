f70a6ff86eb0fa227173a45e5505b9238a40c818
EthanGalbraith <galbraith.92@buckeyemail.osu.edu>
2025-03-04 20:45:41 -0500

    Refactored main, added prepared statements and insert/select placeholder methods.

--- src/main.c
+++ src/main.c
@@ -4,10 +4,29 @@
 #include <string.h>
 
 typedef struct {
-    char* buffer;
-    size_t bufferLength;
-    size_t inputLength;
+	char* buffer;
+	size_t bufferLength;
+	size_t inputLength;
 } InputBuffer;
+
+typedef enum {
+  metaCommandSuccess,
+  metaCommandUnrecognizedCommand
+} MetaCommandResult;
+
+typedef enum {
+  prepareSuccess,
+  prepareUnrecognizedStatement
+} PrepareResult;
+
+typedef enum {
+  statementInsert,
+  statementSelect
+} StatementType;
+
+typedef struct {
+	StatementType type;
+} Statement;
 
 /**
  * @brief prints "db" at the beginning of each line prompting a user command
@@ -47,84 +66,133 @@ InputBuffer *newInputBuffer();
 size_t getline(char **lineptr, size_t *n, FILE *stream);
 
 InputBuffer* newInputBuffer() {
-    InputBuffer* inputBuffer = (InputBuffer*)malloc(sizeof(InputBuffer));
-    inputBuffer->buffer = NULL;
-    inputBuffer->bufferLength = 0;
-    inputBuffer->inputLength = 0;
+	InputBuffer* inputBuffer = (InputBuffer*)malloc(sizeof(InputBuffer));
+	inputBuffer->buffer = NULL;
+	inputBuffer->bufferLength = 0;
+	inputBuffer->inputLength = 0;
 
-    return inputBuffer;
+	return inputBuffer;
 }
 
 void printPrompt() {
-    printf("db > ");
+	printf("db > ");
 }
 
 void readInput (InputBuffer* inputBuffer) {
-    size_t bytesRead = getline(&(inputBuffer->buffer), &(inputBuffer->bufferLength), stdin);
+	size_t bytesRead = getline(&(inputBuffer->buffer), &(inputBuffer->bufferLength), stdin);
 
-    if (bytesRead <= 0) {
-        printf("Error reading input\n");
-        exit(EXIT_FAILURE);
-    }
+	if (bytesRead <= 0) {
+		printf("Error reading input\n");
+		exit(EXIT_FAILURE);
+	}
 
-    //Trim whitespace
-    if (bytesRead > 0 && inputBuffer->buffer[bytesRead - 1] == '\n') {
-        inputBuffer->buffer[bytesRead - 1] = '\0';
-        inputBuffer->inputLength = bytesRead - 1;
-    } else {
-        inputBuffer->inputLength = bytesRead;
-    }    
+	//Trim whitespace
+	if (bytesRead > 0 && inputBuffer->buffer[bytesRead - 1] == '\n') {
+		inputBuffer->buffer[bytesRead - 1] = '\0';
+		inputBuffer->inputLength = bytesRead - 1;
+	} else {
+		inputBuffer->inputLength = bytesRead;
+	}    
 }
 
 void closeInputBuffer(InputBuffer* inputBuffer) {
-    free (inputBuffer->buffer);
-    free (inputBuffer);
+	free (inputBuffer->buffer);
+	free (inputBuffer);
 }
 
 size_t getline(char **lineptr, size_t *n, FILE *stream) {
 
-    //No buffer exists, so allocate memory for one
-    if (*lineptr == NULL) {
-        *n = 128;
-        *lineptr = (char*)malloc(*n);
+	//No buffer exists, so allocate memory for one
+	if (*lineptr == NULL) {
+		*n = 128;
+		*lineptr = (char*)malloc(*n);
 
-        //If malloc fails
-        if (*lineptr == NULL) {
-            return -1;
-        }
+		//If malloc fails
+		if (*lineptr == NULL) {
+			return -1;
+		}
+	}
+
+	int ch;
+	size_t length = 0;
+	while (((ch = fgetc(stream)) != EOF && ch != '\n')) {
+
+		//Double the size if the buffer is full
+		if (length +1 >= *n) {
+			*n *=2;
+			*lineptr = (char*)realloc(*lineptr, *n);
+			if (*lineptr == NULL) {
+				return -1;
+			}
+		}
+		(*lineptr)[length++] = ch;
+	}
+	(*lineptr)[length] = '\0';
+	return length;
+}
+
+MetaCommandResult doMetaCommand(InputBuffer *inputBuffer) {
+	if (strcmp(inputBuffer->buffer, ".exit") == 0) {
+		exit(EXIT_SUCCESS);
+    } else {
+		return metaCommandUnrecognizedCommand;
+	}
+	
+}
+
+PrepareResult prepareStatement(InputBuffer *inputBuffer, Statement *statement) {
+	if (strncmp(inputBuffer->buffer, "insert", 6) == 0) {
+        statement->type = statementInsert;
+        return prepareSuccess;
+	}
+    if (strcmp(inputBuffer->buffer, "select") == 0) {
+        statement->type = statementSelect;
+        return prepareSuccess;
+    } else {
+        return prepareUnrecognizedStatement;
     }
+}
 
-    int ch;
-    size_t length = 0;
-    while (((ch = fgetc(stream)) != EOF && ch != '\n')) {
-
-        //Double the size if the buffer is full
-        if (length +1 >= *n) {
-            *n *=2;
-            *lineptr = (char*)realloc(*lineptr, *n);
-            if (*lineptr == NULL) {
-                return -1;
-            }
-        }
-        (*lineptr)[length++] = ch;
+void executeStatement(Statement *statement) {
+    switch (statement->type) {
+        case (statementInsert):
+            printf("Insert statement placeholder. \n");
+            break;
+        case (statementSelect):
+            printf("Select statement placeholder. \n");
+            break;
     }
-    (*lineptr)[length] = '\0';
-    return length;
 }
 
 
 int main (int argc, char* argv[]) {
-    InputBuffer* inputBuffer = newInputBuffer();
+	InputBuffer* inputBuffer = newInputBuffer();
 
-    while (true) {
-        printPrompt();
-        readInput(inputBuffer);
-        
-        if (strcmp(inputBuffer->buffer, ".exit") == 0) {
-            closeInputBuffer(inputBuffer);
-            exit(EXIT_SUCCESS);
-        } else {
-            printf("Command not recognized '%s'\n", inputBuffer->buffer);
-        }
-    }
+	while (true) {
+		printPrompt();
+		readInput(inputBuffer);
+
+		if (inputBuffer->buffer[0] == '.') {
+			switch (doMetaCommand(inputBuffer)) {
+				case (metaCommandSuccess):
+					continue;
+				case (metaCommandUnrecognizedCommand):
+					printf("Command not recognized: '%s'\n", inputBuffer->buffer);
+					continue;
+			}
+		}
+
+		//Prepared statement
+		Statement statement;
+		switch (prepareStatement(inputBuffer, &statement)) {
+		    case (prepareSuccess):
+			    break;
+		    case (prepareUnrecognizedStatement):
+			    printf("Unrecognized keyword at '%s' \n", inputBuffer->buffer);
+			continue;
+		}
+
+		executeStatement(&statement);
+		printf("Statement executed.\n");
+	}
 }
